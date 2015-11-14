81f592248dd8a78aaaa72d93511ff3c3827fe1d3
dwin357 <e.r.steinmetz@gmail.com>
2015-11-13 18:12:25 -0600

    added tests for post model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :post do
-    
+    title "This is a factory built post"
+    body	"This is some text that comprimises what I think"
   end
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:all) do
+		@post = create(:post)
+	end
+
+	it " has a title attribute" do
+		expect(@post.title).to eq("This is a factory built post")
+	end
+
+	it " has a body attribute" do
+		expect(@post.body).to eq("This is some text that comprimises what I think")
+	end
+
 end
