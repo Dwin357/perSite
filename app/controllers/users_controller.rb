ef840dbd7e8abd96d851500552f1e75ba95a6f45
dwin357 <e.r.steinmetz@gmail.com>
2015-11-13 23:57:42 -0600

    worked out kinks to add user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,11 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(params[:user])
+		@user = User.new(user_params)
 		if @user.save
-			# success
+			redirect_to root_path
 		else
+			@errors = @user.errors.full_messages
 			render "new"
 		end
 	end
@@ -27,6 +28,11 @@ class UsersController < ApplicationController
 
 	# def destroy
 	# end
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :password)
+	end
 
 end
 
